5ef8d1b291d548f5b1983ae11572fe1362fe78d0
pbd-arcadion <pbd.arcadion@gmail.com>
2024-03-26 21:31:35 +0900

    movie info and videos

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,20 +2,21 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   // console.log(path);
 
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <Link href="/">Home</Link> {path === "/" ? " O" : ""}
+          <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
         </li>
         <li>
           <Link href="/about-us">About Us</Link>{" "}
-          {path === "/about-us" ? " O" : ""}
+          {path === "/about-us" ? "🔥" : ""}
         </li>
       </ul>
     </nav>
